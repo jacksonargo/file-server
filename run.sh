f26b8bed35e1182d5c3e6151f80e41d80af91378
Jackson Argo <jackson@jacksonargo.com>
2021-09-23 17:35:17 -0400

    one more cleanup

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ docker build . -t "$IMAGE_NAME"
 docker run --rm \
   --publish 8080:8080 \
   --name "$CONTAINER_NAME" \
-  --volume "${CONTENT_ROOT}:/srv:ro" \
+  --volume "${CONTENT_ROOT}:/srv" \
   --env "FILE_SERVER_LISTEN_ADDRESS=0.0.0.0:8080" \
   --env "FILE_SERVER_CONTENT_ROOT=/srv" \
   "$IMAGE_NAME"
